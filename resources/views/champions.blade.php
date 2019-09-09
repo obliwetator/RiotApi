4ca71073b15b7dcb68817cd7ce71f7db77a11c3f
Patryk <patrkst22@gmail.com>
2019-09-09 18:09:23 +0100

    add temporary <style>. change to dedicated CSS

--- resources/views/champions.blade.php
+++ resources/views/champions.blade.php
@@ -4,52 +4,35 @@
 
 @section('content')
 
-<div id = "champions container">
-@foreach ($champions["keys"] as $item)
-<div id = "individual champions" style="display:inline-block">
-		<a href="#">
-			<div id="image">
+<div id="champions container">
+	@foreach ($champions["keys"] as $keys => $item)
+	<div id="individual champions" style="position: relative; display: inline-block;">
+		<a href="/champion/{{$item}}/statistics">
+			<div id="image" style="position:relative;width:120px;height:120px;border:2px solid #000;">
 				<i class="__sprite __bg120 __bg120-{{$item}}"></i>
 			</div>
 			<div id="name">{{$item}}</div>
-			<div id="role tags">
-				<div id="add role tags"></div>
+			<div id="role tags" style="position: absolute; bottom: 25px; right: 0; text-align: right; line-height: 1">
+				<div id="add role tags" style="display: block;">
+					<span style = "    display: inline-block;
+					line-height: 15px;
+					font-size: 12px;
+					letter-spacing: -0.9px;
+					color: #fff;
+					background: #353b3e;
+					padding: 1px 6px 0 4px;">Top</span>
+				</div>
+				<div id="add role tags" style="display: block;">
+					<span style = "    display: inline-block;
+					line-height: 15px;
+					font-size: 12px;
+					letter-spacing: -0.9px;
+					color: #fff;
+					background: #353b3e;
+					padding: 1px 6px 0 4px;">Middle</span>
+				</div>
 			</div>
 		</a>
 	</div>
-@endforeach
-	<div id = "individual champions" style="display:inline-block">
-		<a href="#">
-			<div id="image">
-				<i class="__sprite __bg120 __bg120-Aatrox"></i>
-			</div>
-			<div id="name">Aatrox</div>
-			<div id="role tags">
-				<div id="add role tags"></div>
-			</div>
-		</a>
-	</div>
-	<div id = "individual champions" style="display:inline-block">
-			<a href="#">
-				<div id="image">
-					<i class="__sprite __bg120 __bg120-Ahri"></i>
-				</div>
-				<div id="name">Ahri</div>
-				<div id="role tags">
-					<div id="add role tags"></div>
-				</div>
-			</a>
-		</div>
-		<div id = "individual champions" style="display:inline-block">
-				<a href="#">
-					<div id="image">
-						<i class="__sprite __bg120 __bg120-Akali"></i>
-					</div>
-					<div id="name">Aatrox</div>
-					<div id="role tags">
-						<div id="add role tags"></div>
-					</div>
-				</a>
-			</div>
-</div>
+	@endforeach
 @endsection
